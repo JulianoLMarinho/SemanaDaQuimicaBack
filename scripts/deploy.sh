40b04bf0a4f724f065be02139a7c91e36d608383
Juliano Marinho <j.delimamarinho@gmail.com>
2024-03-04 13:55:18 -0300

    add scripts to deploy code

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VPS_USERNAME_HOST=${VPS_USERNAME}@${VPS_HOST}
+
+eval $(ssh-agent -s)
+
+ssh-add <(echo "$SSH_PRIVATE_KEY")
+
+ssh ${VPS_USERNAME_HOST} "cd SemanaTeste && rm -rf *"
+
+scp -r app ${VPS_USERNAME_HOST}:~/SemanaTeste
+scp requirements.txt ${VPS_USERNAME_HOST}:~/SemanaTeste
+scp .env ${VPS_USERNAME_HOST}:~/SemanaTeste
+
+ssh ${VPS_USERNAME_HOST} "cd SemanaTeste && python3 -m virtualenv venv"
+ssh ${VPS_USERNAME_HOST} "cd SemanaTeste && source venv/bin/activate && pip install -r requirements.txt"
+
+ssh ${VPS_USERNAME_HOST} "sudo systemctl restart fastapi-test.service"
